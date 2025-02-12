a2be15412e83487058218c59af180a29c60b26f5
Lucas Medeiros <lucas.medeiros.silva2@gmail.com>
2025-02-12 20:21:51 -0300

    Arquivos challenge

---
+++ app.js
@@ -0,0 +1,36 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (!nome) {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    for (let nome of amigos) {
+        lista.innerHTML += `<li>${nome}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("A lista de amigos está vazia. Adicione pelo menos um nome.");
+        return;
+    }
+    
+    let sorteioLista = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[sorteioLista];
+    
+    document.getElementById("resultado").innerHTML = `Amigo sorteado: ${amigoSorteado}`;
+}
